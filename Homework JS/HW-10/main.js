2dd7c01f014bbe31d5fc963e030b0ac7b42f67a0
artem_epelman <ded.kropiva@gmail.com>
2023-03-13 20:14:56 +0200

    initial commit...

--- Homework JS/HW-10/main.js
+++ Homework JS/HW-10/main.js
@@ -1,9 +1,78 @@
+// // є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код,
+// // який при кожному
+// // перезавантажені сторінки буде
+// // додавати до неї +1
 
-// є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде
-// додавати до неї +1
+            // window.onload = function() {
+            //
+            //     let numReload = localStorage.getItem('key') || num
+            //     numReload++
+            //     localStorage.setItem('key', numReload)
+            //
+            //     let div = document.createElement('div')
+            //     document.body.append(div)
+            //
+            //     div.append(numReload.toString())
+            // }
 
+
+// Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions
+// зберігається інформація про дату та час відвідування сторінки. Є ще сторінка sessions.html
+// (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html.
+//     Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
+
+
+let sJSON = (localStorage.getItem('key') || [])
+
+s = JSON.parse(sJSON)
+
+
+
+s.push(Date.now())
 
 let div = document.createElement('div')
-document.body.append(div)
-let number = 0
+div.style.width = '100px'
+div.style.height = '100px'
+div.style.margin = '20px'
+div.style.background = 'silver'
+
+
+
+document.body.appendChild(div)
+
+div.append(`Now is ${s}`)
+
+localStorage.setItem('key', JSON.stringify(s))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Homework JS/additional/tasks4_additional/main.js
@@ -0,0 +1,42 @@
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+
+            function minNum(a, b, c) {
+                if (a < b && a < c) {
+                    console.log(a)
+                } else if (b < a && b < c) {
+                    console.log(b)
+            } else if (c < a && c < b) {
+                    console.log(c)
+                }
+            }
+            minNum(3,2,1,)
+
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+
+            function maxNum(a, b, c) {
+                if (a > b && a > c) {
+                    console.log(a)
+                } else if (b > a && b > c) {
+                    console.log(b)
+                } else if (c > a && c > b) {
+                    console.log(c)
+                }
+            }
+            maxNum(3,2,1,)
+
+            // - створити функцію яка повертає найбільше число з масиву
+
+let array = [1,2,3,4,5,6,4,5,3,6,54,56,76,43,45,5,7,4,3,23]
+
+function biggestNum(arr) {
+    if (arr.length === arr.length)
+        console.log(arr)
+        console.log(arr.lastOf())
+}
+
+biggestNum(array)
+
+
+
+
