05d05d22be5148960b586fd0714a0c757d745776
artem_epelman <ded.kropiva@gmail.com>
2023-04-02 19:59:50 +0300

    initial commit...

--- Homework JS/HW-12/main.js
+++ Homework JS/HW-12/main.js
@@ -4,30 +4,21 @@
 // при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
 // отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   ХХХ - айді користувача)
 
-
-
-
-
-fetch('https://jsonplaceholder.typicode.com/users') // робимо запит
+let usersUrl = new URL('https://jsonplaceholder.typicode.com/users')
+fetch(usersUrl) // робимо запит
     .then(obj => obj.json())
-    .then(result => {
-        for (let i of result) { // будуємо DOM структуру
+        .then(result => {
 
+        for (let i of result) { // будуємо DOM структуру
             let id = i.id
             let name = i.name
-
             let a = document.createElement('a')
-            document.body.append(a)
             a.innerText = `${id} ${name}`
             a.style.display = 'block'
 
 
-            detailsUrl.searchParams.append('id', i.id)
-            // a.href = // узнать как перенестись на другую сьраницу по ссылке
-                detailsUrl.searchParams.append('id', i.id)
-            detailsUrl.searchParams.append('id', i.id)
-            a.addEventListener('click', {
 
-            })
+            a.href = `user-details.html?id=${id}` // формируем ссылку + id
+            document.body.append(a)
         }
     })
